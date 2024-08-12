7891fb3e85291e08aefe4c689f8171998fc68b52
kareem <kareemwally@std.mans.edu.eg>
2024-08-12 03:54:23 +0300

    changing templates

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,7 +7,6 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -19,27 +18,11 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/0-hbnb/')
 def hbnb():
-    """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    return render_template('0-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places, cache_id=uuid.uuid4())
-
+    """ Return the 0-hbnb.html template with cache_id """
+    cache_id = uuid.uuid4()
+    return render_template('0-hbnb.html', cache_id=cache_id)
 
 if __name__ == "__main__":
     """ Main Function """
